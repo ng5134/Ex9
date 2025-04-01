e68d5997a1ea2dceaebc17805cdd5e2b48d3e3c7
ng5134 <ng5134@bs.amalnet.k12.il>
2025-04-01 16:55:01 +0300

    java changes

--- app/src/main/java/com/example/ex9/MainActivity.java
+++ app/src/main/java/com/example/ex9/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex9;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +14,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText eWeb;
+    Button click ;
+    WebView wV;
+    String stringUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wV=(WebView) findViewById(R.id.wV);
+        eWeb=(EditText) findViewById(R.id.eWeb);
+        click= (Button) findViewById(R.id.click);
+
+        wV.getSettings().setJavaScriptEnabled(true);
+        wV.loadUrl(stringUrl);
+        wV.setWebViewClient(new MyWebViewClient());
+
+    }
+
+    public void go(View view) {
+        stringUrl= eWeb.getText().toString();
+        wV.loadUrl(stringUrl);
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
     }
 }
